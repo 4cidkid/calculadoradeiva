02eb7922db464a2e3f64cfe461c92a7b6aab5d56
Nicolas Torres <desmondgamesyt@gmail.com>
2023-02-12 14:30:27 -0300

    added a function to calculate the IVA of the entered amount

--- src/Components/Calculadora/Calculadora.js
+++ src/Components/Calculadora/Calculadora.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calculadora.css'
 
 
 
 export default function Calculadora(){
+    const[monto,setMonto] = useState()
+    const[finalPrice, setfinalPrice] = useState()
+    function calculateIva(e){
+        let liveMonto = e.target.value;
+        liveMonto = liveMonto * 0.21;
+        let calculateFinal = parseInt(e.target.value) + liveMonto
+        setMonto(liveMonto)
+        setfinalPrice(calculateFinal)
+    }
     return(
         <div>
-            <input type="number" />
+            <label htmlFor='monto'>Ingresa el monto:</label>
+            <input id='monto' name='monto' type="number" onChange={calculateIva} />
+            <label>El iva es: {monto !== 0 ? monto : undefined}</label>
+            <label>El precio final es {finalPrice}</label>
         </div>
     )
 }
