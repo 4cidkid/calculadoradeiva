8a9b9c666b6642fd2dfc5def40b835ef120bb9f1
Nicolas Torres <desmondgamesyt@gmail.com>
2023-02-12 17:19:39 -0300

    added a validation for input, now user can only put numbers and not any type of symbols

--- src/Components/Calculadora/Calculadora.js
+++ src/Components/Calculadora/Calculadora.js
@@ -2,35 +2,47 @@ import React, { useState } from "react";
 import "./Calculadora.css";
 
 export default function Calculadora() {
-  const [monto, setMonto] = useState();
-  const [finalPrice, setfinalPrice] = useState();
+  const [monto, setMonto] = useState(0);
+  const [finalPrice, setfinalPrice] = useState(0);
+  function validateInput(e){
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+  }
   function calculateIva(e) {
-    if(parseInt(e.target.value) < 0){
-        setMonto('El monto tiene que ser positivo');
-        setfinalPrice('El monto tiene que ser positivo')
-    }else{
+      if(e.target.value<0){
+        setMonto(0)
+        finalPrice(0)
+      }else{
         let liveMonto = e.target.value;
         liveMonto = liveMonto * 0.21;
-        let calculateFinal = liveMonto? parseInt(e.target.value) + liveMonto:undefined
-        setMonto(liveMonto);
-        setfinalPrice(calculateFinal);
-    }
+        let calculateFinal = liveMonto
+          ? parseInt(e.target.value) + liveMonto
+          : undefined;
+        setMonto(liveMonto.toFixed(0));
+        setfinalPrice(calculateFinal? calculateFinal.toFixed(0) : 0);
+      }
   }
   return (
     <div className="calculadora">
       <div className="calculadora-container">
         <h1 id="main-title">Calculadora de IVA</h1>
-        <div>
+        <div className="monto-container">
           <label htmlFor="monto">Ingresa el monto:</label>
           <input
             id="monto"
             name="monto"
             type="number"
             onChange={calculateIva}
+            onInput={validateInput}
           />
         </div>
-        <label>El iva es: {monto !== 0 ? monto : undefined}</label>
-        <label>El precio final es {finalPrice}</label>
+        <div className="result">
+          <label>El iva es:</label>
+          <h2 style={{margin: '0px'}} >{monto}</h2>
+        </div>
+        <div className="result">
+          <label>El precio final es:</label>
+          <h2 style={{margin: '0px'}} >{finalPrice}</h2>
+        </div>
       </div>
     </div>
   );
