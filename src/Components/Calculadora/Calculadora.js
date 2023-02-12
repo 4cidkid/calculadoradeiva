049af5a5e9533fe3a8a7ab21152563f2f7da29c2
Nicolas Torres <desmondgamesyt@gmail.com>
2023-02-12 15:42:04 -0300

    added conditionaly statement to check if liveMonto exist, if not calculateFinal is undefined and its not rendered.

--- src/Components/Calculadora/Calculadora.js
+++ src/Components/Calculadora/Calculadora.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
-import './Calculadora.css'
+import React, { useState } from "react";
+import "./Calculadora.css";
 
-
-
-export default function Calculadora(){
-    const[monto,setMonto] = useState()
-    const[finalPrice, setfinalPrice] = useState()
-    function calculateIva(e){
+export default function Calculadora() {
+  const [monto, setMonto] = useState();
+  const [finalPrice, setfinalPrice] = useState();
+  function calculateIva(e) {
+    if(parseInt(e.target.value) < 0){
+        setMonto('El monto tiene que ser positivo');
+        setfinalPrice('El monto tiene que ser positivo')
+    }else{
         let liveMonto = e.target.value;
         liveMonto = liveMonto * 0.21;
-        let calculateFinal = parseInt(e.target.value) + liveMonto
-        setMonto(liveMonto)
-        setfinalPrice(calculateFinal)
+        let calculateFinal = liveMonto? parseInt(e.target.value) + liveMonto:undefined
+        setMonto(liveMonto);
+        setfinalPrice(calculateFinal);
     }
-    return(
+  }
+  return (
+    <div className="calculadora">
+      <div className="calculadora-container">
+        <h1 id="main-title">Calculadora de IVA</h1>
         <div>
-            <h1>Calculadora de IVA</h1>
-            <label htmlFor='monto'>Ingresa el monto:</label>
-            <input id='monto' name='monto' type="number" onChange={calculateIva} />
-            <label>El iva es: {monto !== 0 ? monto : undefined}</label>
-            <label>El precio final es {finalPrice}</label>
+          <label htmlFor="monto">Ingresa el monto:</label>
+          <input
+            id="monto"
+            name="monto"
+            type="number"
+            onChange={calculateIva}
+          />
         </div>
-    )
+        <label>El iva es: {monto !== 0 ? monto : undefined}</label>
+        <label>El precio final es {finalPrice}</label>
+      </div>
+    </div>
+  );
 }
